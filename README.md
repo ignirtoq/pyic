c5939eef084257d19eb53119a09afb7b39471b3e
Jeffrey Bouas <ignirtoq@gmail.com>
2020-08-14 19:42:07 -0400

    Rename to prepare for more frontends.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# python-in-slack
-A Slack app providing Python interpreter sessions.
+# pyic: Python Interpreters for Chat
+pyic is an MIT-licensed application for integrating Python interpreters into
+your chat system of choice.
+
+pyic puts a bot (pybot) in each desired channel that watches for code blocks.
+When it sees one, it runs the code block in a Python interpreter and prints
+the results as a reply to the thread (if supported), in the channel, or as
+a response to a direct message.
+
+## Features
+* Isolated interpreters for each channel (or user for direct messages)
+* Modular design, so adding new front-ends for new chat clients is simple.
+* Supported chat systems:
+    * Slack
--- setup.py
+++ setup.py
@@ -1,20 +1,10 @@
-import codecs
-import os
+from pathlib import Path
 import re
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(*parts):
-    # intentionally *not* adding an encoding option to open, See:
-    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with codecs.open(os.path.join(here, *parts), 'r') as fp:
-        return fp.read()
-
 
 def find_version(*file_paths):
-    version_file = read(*file_paths)
+    version_file = Path(__file__).parent.joinpath(*file_paths).read_text()
     version_match = re.search(
         r"^__version__ = ['\"]([^'\"]*)['\"]",
         version_file,
@@ -26,11 +16,13 @@ def find_version(*file_paths):
     raise RuntimeError('Unable to find version string.')
 
 
-NAME = 'pyslackapp'
+NAME = 'pyic'
 VERSION = find_version(NAME, '__init__.py')
 LICENSE = 'MIT'
+DESCRIPTION = 'Python In Chat'
+LONG_DESCRIPTION = Path(__file__).parent.joinpath('README.md').read_text()
 AUTHOR = 'Jeffrey Bouas'
-EMAIL = 'ignirtoq+pyslackapp@gmail.com'
+EMAIL = 'ignirtoq+pyic@gmail.com'
 PACKAGES = find_packages()
 INSTALL_REQUIRES = [
     'aiohttp',
@@ -55,6 +47,8 @@ setup(
     name=NAME,
     author=AUTHOR,
     author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     version=VERSION,
     license=LICENSE,
     packages=PACKAGES,
